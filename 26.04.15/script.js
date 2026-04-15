7de8b064910d648709e85c3bbec8b9846da895bc
AdamMakay <makayadam1@gmail.com>
2026-04-15 14:41:39 +0200

    Add files via upload

---
+++ 26.04.15/script.js
@@ -0,0 +1,7 @@
+function valtoztatas(){
+    let valasztott = document.getElementsByClassName("settings");
+    let kep = document.getElementById("szia");
+    if (valasztott == "center"){
+        kep.style.alignContent = "center";
+    }
+}
