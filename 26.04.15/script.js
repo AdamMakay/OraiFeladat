ed88fcebf6737851f67e4b76f5b4034157e94cbe
AdamMakay <makayadam1@gmail.com>
2026-04-22 09:58:18 +0200

    update

--- 26.04.15/script.js
+++ 26.04.15/script.js
@@ -1,7 +1,8 @@
 function valtoztatas(){
-    let valasztott = document.getElementsByClassName("settings");
-    let kep = document.getElementById("szia");
-    if (valasztott == "center"){
-        kep.style.alignContent = "center";
-    }
+    var szia = document.getElementById("szia")
+    var alignContent = document.getElementById("align-content").value
+    var alignitem = document.getElementById("align-items").value
+    szia.style.alignContent = alignContent
+    szia.style.alignItems = alignitem
+    
 }
